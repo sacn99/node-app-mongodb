1a7af4cba61f3ebc56c09b8582e5843ac3142c35
Sergio Cabrera <sergiocabreranieto@gmail.com>
2022-11-09 17:20:52 -0500

    views and static files folder path redefined

---
+++ src/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+
+//Initializations
+const app = express();
+
+//Settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+
+//Middlewares
+app.use(express.urlencoded({extended: false}));
+
+//Global Variablesimage
+
+//Routes
+app.get('/', (req, res)=>{
+    res.send('Hello world');
+})
+
+//Static Files
+app.use(express.static(path.join(__dirname, 'public')))
+
+module.exports = app;
